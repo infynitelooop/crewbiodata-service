0392be140c29a70db483a222e4855e8a738264d7
Sanjay Ranga <sanjay.ranga.au@gmail.com>
2026-05-20 23:13:25 +1000

    Initial commit

---
+++ src/main/java/com/infyniteloop/crewbiodata/CrewbiodataApplication.java
@@ -0,0 +1,13 @@
+package com.infyniteloop.crewbiodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewbiodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewbiodataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infyniteloop/crewbiodata/CrewbiodataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infyniteloop.crewbiodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewbiodataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
